a633e376a179f715a66f55ff37485a8b4c0af01d
Lúcio Brito <luciopereirabritoo@gmail.com>
2026-09-20 23:05:06 -0300

    test(description): improve the unit tests
    It is necessary to reorganize the unit tests to improve understanding. Plus, it
    adds some unit tests.

--- task-manager-frontend/src/app/components/inputs/description/description.spec.ts
+++ task-manager-frontend/src/app/components/inputs/description/description.spec.ts
@@ -16,30 +16,62 @@ describe('Description', () => {
     await fixture.whenStable();
   });
 
-  it('should initialize without value', () => {
-    //#region ARRANGES
-    //#endregion
+  describe('Behavior', () => {
+    it('should initialize without value', () => {
+      //#region ARRANGES
+      //#endregion
 
-    //#region ACTIONS
-    //#endregion
+      //#region ACTIONS
+      //#endregion
 
-    //#region ASSERTIONS
-    expect(component.form.value).toBe('');
-    //#endregion
+      //#region ASSERTIONS
+      expect(component.form.value).toBe('');
+      //#endregion
+    });
+
+    it('should accept until 250 characters', () => {
+      //#region ARRANGES
+      const mockValue = 'a'.repeat(250);
+      //#endregion
+
+      //#region ACTIONS
+      component.form.setValue(mockValue);
+      //#endregion
+
+      //#region ASSERTIONS
+      expect((component.form.value as string).length).toBeLessThanOrEqual(250);
+      expect(component.form.valid).toBeTruthy();
+      //#endregion
+    });
+
+    it('should not accept more than 250 characters', () => {
+      //#region ARRANGES
+      const mockValue = `a`.repeat(251);
+      //#endregion
+
+      //#region ACTIONS
+      component.form.setValue(mockValue);
+      //#endregion
+
+      //#region ASSERTIONS
+      expect(component.form.valid).toBeFalsy()
+      //#endregion
+    });
   });
 
-  it('should accept until 250 characters', () => {
-    //#region ARRANGES
-    const mockValue = 'a'.repeat(250);
-    //#endregion
+  describe('UI', () => {
+    it('should renders description input', () => {
+      //#region ARRANGES
+      const html = fixture.nativeElement as HTMLElement;
+      const textAreaDescription = html.querySelector('[data_test-textAreaDescription]') as HTMLTextAreaElement;
+      //#endregion
 
-    //#region ACTIONS
-    component.form.setValue(mockValue);
-    //#endregion
+      //#region ACTIONS
+      //#endregion
 
-    //#region ASSERTIONS
-    expect((component.form.value as string).length).toBeLessThanOrEqual(250);
-    expect(component.form.valid).toBeTruthy();
-    //#endregion
+      //#region ASSERTIONS
+      expect(textAreaDescription).not.toBeNull()
+      //#endregion
+    });
   });
 });
